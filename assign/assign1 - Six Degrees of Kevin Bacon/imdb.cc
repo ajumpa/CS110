1ead0aa3372136af7f90682bab51328452d462ac
ajumpa <aaronjumpa@gmail.com>
2023-04-16 17:40:22 -0700

    Seg faults in some cases

--- assign/assign1 - Six Degrees of Kevin Bacon/imdb.cc
+++ assign/assign1 - Six Degrees of Kevin Bacon/imdb.cc
@@ -29,11 +29,6 @@ imdb::~imdb() {
 	releaseFileMap(movieInfo);
 }
 
-/*
-	Binary search for actor name
-	Get moviedata offset bytes from end of actor record
-	populate film vecotr with film data from moviedata
-*/
 bool imdb::getCredits(const string& player, vector<film>& films) const { 
   const int *lx = (int *) actorFile + 1;
 	const int *hx = lx + ((int *) this->actorFile)[0];
@@ -48,34 +43,60 @@ bool imdb::getCredits(const string& player, vector<film>& films) const {
 	if (strcmp(player.c_str(), name) != 0)
 		return 0;
 	
-	printf("Found %s at offset %d\n", name, *pos);
+	int ix = *pos + strlen(name) + ((strlen(name) % 2) ?  1 : 2);
 
-	int name_len = strlen(name);
-	int c = ( (name_len % 2) ?  1 : 2);
-	int ix = *pos+name_len+c;
-	short num_movies = (short) ((unsigned char *) this->actorFile)[ix];
+	short num_movies = (short) ((char *) this->actorFile)[ix];
 
-	printf("Number of movies %hi\n", num_movies);
+	ix += ix%4;
 
-	ix += 2;
-
-	for (int i = 0; i <= (int) num_movies; i++)
+	int *offset_list = (int *) ((char *) actorFile + ix);
+	for (int i = 1; i <= (int) num_movies; i++)
 	{
-		printf("%d \n",  *(int * ) &((int *) this->actorFile)[ix+i*4] );
+		int movie_offset = *(offset_list + i);
+		char *title = ((char *) movieFile) + movie_offset;
+		int year =  (int) *((char *) movieFile + movie_offset + strlen(title) + 1) + 1900;
 
-		/**
-		 *  
-		const char *movie_name = &((char *) this->movieFile)[*movie_ix];
-		printf("HELLERRRR\n");
-		printf("%s\n", &movie_name);
-		 * 
-		 */
+		film new_film = film {title, year};
+		films.push_back(new_film);
 	}
-	return 0;
+
+	return 1;
 }
 
 bool imdb::getCast(const film& movie, vector<string>& players) const { 
-	return 0;
+	char *title = (char *) (movie.title).c_str();
+  const int *lx = (int *) movieFile + 1;
+	const int *hx = lx + ((int *) this->movieFile)[0];
+	
+	const int *pos = lower_bound(lx, hx, title, [this] (int movie_ix, const std::string& title) {
+		int cmp = strcmp(&((char *) this->movieFile)[movie_ix], title.c_str());
+		return cmp < 0 ? true : false;
+	});
+
+	const char *movie_name = &((char *) this->movieFile)[*pos];
+
+	if (strcmp(title, movie_name) != 0)
+		return 0;
+
+	printf("Movie : %s\n", movie_name);
+
+	int ix = *pos + (strlen(movie_name)+1) + 1 + (((strlen(movie_name) + 2) % 2) ?  1 : 0);
+
+	short num_actors = (short) ((char *) this->movieFile)[ix];
+
+	printf("%d actors :\n", num_actors);
+	
+	ix += ix%4;
+
+	int *offset_list = (int *) ((char *) movieFile + ix);
+	for (int i = 1; i <= (int) num_actors; i++)
+	{
+		int offset = *(offset_list + i);
+		char *name = ((char *) actorFile) + offset;
+		printf("\t%d. %s\n", i, name);
+	}
+
+	return 1;
 }
 
 const void *imdb::acquireFileMap(const string& fileName, struct fileInfo& info) {
