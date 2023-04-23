d5e6ffbd96b914d5a5f951ef67aadfe5d4228497
ajumpa <aaronjumpa@gmail.com>
2023-04-23 13:50:05 -0700

    Inode seg fault

--- assign/assign2 - Reading Unix v6 File Systems/inode.c
+++ assign/assign2 - Reading Unix v6 File Systems/inode.c
@@ -5,16 +5,22 @@
 #include "diskimg.h"
 
 #define INDIR_ADDR 7
+#define INODES_PER_BLOCK DISKIMG_SECTOR_SIZE/sizeof(struct inode) 
 
-/**
- */
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
+  struct inode i_nodes [INODES_PER_BLOCK];
+  if (diskimg_readsector(fs->dfd, INODE_START_SECTOR + inumber/INODES_PER_BLOCK, &i_nodes) == -1)
+  {
+    fprintf(stderr, "Error reading inode data\n");
+    return -1;
+  }
+  inp = &i_nodes[inumber];
+  printf("%d :\n", inode_getsize(inp));
+
   return 0;
 }
 
 
-/**
- */
 int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {
   return 0;
 }
