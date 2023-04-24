d9c201d2d2866381ea53203ed4128978f3a1c8c9
ajumpa <aaronjumpa@gmail.com>
2023-04-23 20:36:08 -0700

    I think this works, hard to tell

--- assign/assign2 - Reading Unix v6 File Systems/inode.c
+++ assign/assign2 - Reading Unix v6 File Systems/inode.c
@@ -3,28 +3,25 @@
 
 #include "inode.h"
 #include "diskimg.h"
+#include <string.h>
 
 #define INDIR_ADDR 7
-#define INODES_PER_BLOCK DISKIMG_SECTOR_SIZE/sizeof(struct inode) 
+#define INODES_PER_BLOCK 16
 
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
-  struct inode i_nodes [INODES_PER_BLOCK];
-  if (diskimg_readsector(fs->dfd, INODE_START_SECTOR + inumber/INODES_PER_BLOCK, &i_nodes) == -1)
+  struct inode i_nodes[INODES_PER_BLOCK];
+  if (diskimg_readsector(fs->dfd, INODE_START_SECTOR + ((inumber-1)/INODES_PER_BLOCK), &i_nodes) == -1)
   {
     fprintf(stderr, "Error reading inode data\n");
     return -1;
   }
-  inp = &i_nodes[inumber];
-  printf("%d :\n", inode_getsize(inp));
-
+  memcpy(inp, &i_nodes[(inumber-1)%INODES_PER_BLOCK], sizeof(struct inode) );
   return 0;
 }
-
 
 int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {
   return 0;
 }
-
 
 /**
  * Computes the size in bytes of the file identified by the given inode
