2662ee37c4c88a4b3fc2cd34680e1cf9a48a9fce
ajumpa <aaronjumpa@gmail.com>
2023-04-24 14:06:31 -0700

    Direct addressing works... I think

--- assign/assign2 - Reading Unix v6 File Systems/inode.c
+++ assign/assign2 - Reading Unix v6 File Systems/inode.c
@@ -8,19 +8,51 @@
 #define INDIR_ADDR 7
 #define INODES_PER_BLOCK 16
 
+/**
+ * Fetches the specified inode from the filesystem. 
+ * Returns 0 on success, -1 on error.  
+ */
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
   struct inode i_nodes[INODES_PER_BLOCK];
-  if (diskimg_readsector(fs->dfd, INODE_START_SECTOR + ((inumber-1)/INODES_PER_BLOCK), &i_nodes) == -1)
+  int sector_offset = ((inumber-1)/INODES_PER_BLOCK);
+  int node_offset = (inumber-1)%INODES_PER_BLOCK;
+
+  if (diskimg_readsector(fs->dfd, INODE_START_SECTOR + sector_offset, i_nodes) < 0)
   {
-    fprintf(stderr, "Error reading inode data\n");
     return -1;
   }
-  memcpy(inp, &i_nodes[(inumber-1)%INODES_PER_BLOCK], sizeof(struct inode) );
-  return 0;
+  
+  memcpy(inp, &i_nodes[node_offset], sizeof(struct inode) );
+
+  return inumber;
 }
 
+/**
+ * Given an index of a file block, retrieves the file's actual block number
+ * of from the given inode.
+ *
+ * Returns the disk block number on success, -1 on error.  
+ */
 int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {
-  return 0;
+  
+  int sect_num = 0;
+  int size = inode_getsize(inp);
+  int large = (inp->i_mode & ILARG);
+  
+
+  // Direct addressing
+  if ( !(large) )
+  {
+    sect_num = inp->i_addr[blockNum];
+  }
+
+  else
+  {
+
+  }
+
+
+  return sect_num;
 }
 
 /**
