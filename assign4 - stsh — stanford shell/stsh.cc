e94ef16f56299f25ef079726a566eb1aee17a1d1
ajumpa <aaronjumpa@gmail.com>
2023-05-20 15:41:03 -0700

    need to add fg built in, also implement process status to running correctly

--- assign4 - stsh — stanford shell/stsh.cc
+++ assign4 - stsh — stanford shell/stsh.cc
@@ -35,11 +35,66 @@ static bool handleBuiltin(const pipeline& pipeline) {
   switch (index) {
     case 0:
     case 1: exit(0);
+    case 2: foreground();
     case 7: cout << joblist; break;
     default: throw STSHException("Internal Error: Builtin command not supported."); // or not implemented yet
   }
 
   return true;
+}
+
+static void foreground()
+{
+
+}
+
+static void childStatusHandler(int sig)
+{
+  while(true)
+  {
+    int status;
+    pid_t pid = waitpid(-1, &status, WUNTRACED|WCONTINUED|WNOHANG);
+    if (pid <= 0) break;
+    if (joblist.containsProcess(pid))
+    {
+      STSHJob& job = joblist.getJobWithProcess(pid);
+      STSHProcess& process = job.getProcess(pid);
+      if (WIFEXITED(status) || WIFSIGNALED(status))
+        process.setState(kTerminated);
+      if (WIFSTOPPED(status))
+        process.setState(kStopped); 
+      if (WIFCONTINUED(status))
+        process.setState(kRunning);
+      joblist.synchronize(job);
+    }
+  }
+}
+
+// TODO: HANDLE JOBS BEING BROUGHT BACK TO FOREGROUND
+static void contHandler(int sig)
+{
+  
+}
+
+static void stopHandler(int sig)
+{
+  if (joblist.hasForegroundJob())
+  {
+    STSHJob& job = joblist.getForegroundJob();
+    job.setState(kBackground);
+    kill(job.getGroupID(), SIGTSTP);
+    joblist.synchronize(job);
+  }
+}
+
+static void intHandler(int sig)
+{
+  if (joblist.hasForegroundJob())
+  {
+    STSHJob& job = joblist.getForegroundJob();
+    kill(job.getGroupID(), SIGKILL);
+    joblist.synchronize(job);
+  }
 }
 
 /**
@@ -54,6 +109,66 @@ static void installSignalHandlers() {
   installSignalHandler(SIGQUIT, [](int sig) { exit(0); });
   installSignalHandler(SIGTTIN, SIG_IGN);
   installSignalHandler(SIGTTOU, SIG_IGN);
+  signal(SIGCHLD, childStatusHandler);
+  signal(SIGINT, intHandler);
+  signal(SIGTSTP, stopHandler);
+  signal(SIGCONT, contHandler);
+}
+
+/* Add job to joblist with list of processes that are waiting to run
+*/
+static size_t addToJobList(const vector<STSHProcess>& processes, STSHJobState state) {
+  STSHJob& job = joblist.addJob(state);
+  for (const STSHProcess& process : processes) job.addProcess(process);
+  return job.getNum();
+}
+
+/**
+ * Create a list of processes from a parsed command
+*/
+static vector<STSHProcess> createProcesses(const pipeline& p)
+{
+  vector<STSHProcess> jobProcesses;
+  for (size_t i = 0; i < p.commands.size(); i++)
+  {
+    char *argv[kMaxArguments + 2];
+    argv[0] = (char *) p.commands[i].command;
+    
+    size_t j;
+    for (j = 0; j <= kMaxArguments || p.commands[i].tokens[j] == NULL; j++) {
+      argv[j+1] = p.commands[i].tokens[j];
+    }
+
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+     execvp(argv[0], argv);
+    }
+    STSHProcess process = STSHProcess(pid, p.commands[i], kWaiting);
+    jobProcesses.push_back(process);
+  }
+  return jobProcesses;
+}
+
+static void syncJobList(size_t newJobNum)
+{
+  sigset_t mask, oldMask;
+  sigemptyset(&oldMask);
+  sigaddset(&oldMask, SIGCHLD);
+  sigprocmask(SIG_BLOCK, &oldMask, &mask);
+
+
+  // THIS IS WEIRD NEEDS TO BE DONE RIGHT
+  STSHJob& job = joblist.getJob(newJobNum);
+  while(job.getState() == kForeground)  
+  {
+    sigsuspend(&mask);
+    STSHProcess& process = job.getProcesses().front();
+    setpgid(process.getID(), 0);
+    process.setState(kRunning);
+    joblist.synchronize(job);
+  }
+  sigprocmask(SIG_UNBLOCK, &oldMask, NULL);
 }
 
 /**
@@ -62,33 +177,11 @@ static void installSignalHandlers() {
  *  * Creates a new job on behalf of the provided pipeline.
  *  */
 static void createJob(const pipeline& p) {
-  // A job will have a pipeline
-  // A pipeline will have 1 or more commands
-  // each with 0 or more arguments
-
-  //STSHJob& job = joblist.addJob(kForeground);
-
-  for (size_t i = 0; i < p.commands.size(); i++)
-  {
-    for (size_t j = 0; j <= kMaxArguments && p.commands[i].tokens[j] != NULL; j++) {
-
-    }
-
-    pid_t pid = fork();
-    
-    // TODO: tokens not being passed sto execvp for some reason
-    if (pid == 0)
-    {
-     execvp(p.commands[i].command, p.commands[i].tokens);
-    }
-
-    else
-    {
-      int status;
-      if (waitpid(pid, &status, 0) == -1)
-        perror("waitpid");
-    }
-  }
+  vector<STSHProcess> jobProcesses = createProcesses(p);
+  //bool inForeground = !joblist.hasForegroundJob();
+  //enum STSHJobState newJobState = inForeground ? kForeground : kBackground;
+  size_t newJobNum = addToJobList(jobProcesses, kForeground);
+  syncJobList(newJobNum);
 }
 
 /**
